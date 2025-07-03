fac14dd0a003fd63903ffd7e4a63a183b057d9ae
AmoghaK1 <amogha.khare@gmail.com>
2025-07-03 22:55:20 +0530

    same bug fix again

--- services/loginService.js
+++ services/loginService.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const userVerification = require('../models/userVerification');
 const crypto = require('crypto');
-const transporter = require('../config/NodeMailer');
+const transporter = require('../config/nodeMailer');
 const { sendVerification } = require('../services/emailService');
 const PaymentStatus = require('../models/paymentModel');
 

012c33f260238b99298815036ebedc8b87d371b9
AmoghaK1 <amogha.khare@gmail.com>
2025-10-04 20:44:41 +0530

    fixed bugs

--- config/sendgrid.js
+++ config/sendgrid.js
@@ -10,22 +10,4 @@ if (!process.env.SENDGRID_API_KEY) {
     console.log('SendGrid ready');
 }
 
-module.exports = sgMail;require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.AUTH_EMAIL,
-        pass: process.env.AUTH_PASS
-    }
-});
-
-transporter.verify((error, success) => {
-    if (error) {
-        console.error("Nodemailer config error:", error);
-    } else {
-        console.log('Nodemailer ready');
-    }
-});
-
-module.exports = transporter;
+module.exports = sgMail;
